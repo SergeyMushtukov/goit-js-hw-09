f5f080150bbf8b659a76a03299d90755de23e3d3
SergeyMushtukov <122204519+SergeyMushtukov@users.noreply.github.com>
2023-06-04 12:47:22 +0000

    Deploying to gh-pages from @ SergeyMushtukov/goit-js-hw-09@6df1270706d6645a1dc74d7c245f9baf095e752c 🚀

---
+++ 01-color-switcher.45a7e8cf.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),r(e),a(n)})),n.addEventListener("click",(function(){clearInterval(o),r(n),a(e)})),r(n);var o=null;function r(t){t.setAttribute("disabled"," ")}function a(t){t.removeAttribute("disabled")}}();
+//# sourceMappingURL=01-color-switcher.45a7e8cf.js.map
